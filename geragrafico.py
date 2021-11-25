9729c3ff2f92d32ad191c142ed7588d1271e9d63
Yasmin Souza Camargo <yasmincamargo012@gmail.com>
2021-11-25 17:54:34 -0300

    Inserindo gráfico com a média das séries

--- geragrafico.py
+++ geragrafico.py
@@ -8,6 +8,8 @@ def main():
     valores = leitor.getValores()
     print(valores)
     
+    plt.subplot(1, 2, 1)
+    
     plt.xlabel('Amostragem')
     plt.ylabel('Valores de entrada')
     plt.title('Gráfico de linhas')
@@ -15,6 +17,14 @@ def main():
     for i in range(len(valores)):
         plt.plot(valores[i])
         
+    plt.subplot(1, 2, 2)
+    medias = leitor.calculoMedias()
+    xvalues = np.arange(1, len(medias) + 1)
+    plt.bar(xvalues, medias)
+    plt.xticks(xvalues, ['Séries ' + str(x) for x in xvalues])
+    
+    plt.title('Média das séries')
+    
     plt.show()
     
 main()
